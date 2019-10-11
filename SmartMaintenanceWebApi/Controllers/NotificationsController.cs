23d71b40ca71ec803d0d0ca75239f316fbaf77be
Chua Wei Zhe <chuazhe@gmail.com>
2019-10-12 00:36:43 +0800

    Add Notifications

---
+++ SmartMaintenanceWebApi/Controllers/NotificationsController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Web;
+using System.Net.Http;
+using System.Net;
+using SmartMaintenanceWebApi.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SmartMaintenanceWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class NotificationsController : Controller
+    {
+        // GET: api/<controller>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        /*
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value"+id;
+
+        }
+        */
+
+        [HttpPost("zzz")]
+        public async Task<HttpResponseMessage> Post2()
+        {
+            var user = "Test";
+            var pns = "fcm";
+            var message = "Test 2";
+
+            Microsoft.Azure.NotificationHubs.NotificationOutcome outcome = null;
+            HttpStatusCode ret = HttpStatusCode.InternalServerError;
+
+            switch (pns.ToLower())
+            {
+                case "wns":
+                    // Windows 8.1 / Windows Phone 8.1
+                    var toast = @"<toast><visual><binding template=""ToastText01""><text id=""1"">" +
+                                "From " + user + ": " + message + "</text></binding></visual></toast>";
+                    outcome = await Notifications.Instance.Hub.SendWindowsNativeNotificationAsync(toast);
+                    break;
+                case "apns":
+                    // iOS
+                    var alert = "{\"aps\":{\"alert\":\"" + "From " + user + ": " + message + "\"}}";
+                    outcome = await Notifications.Instance.Hub.SendAppleNativeNotificationAsync(alert);
+                    break;
+                case "fcm":
+                    // Android
+                    var notif = "{ \"data\" : {\"message\":\"" + "From " + user + ": " + message + "\"}}";
+                    outcome = await Notifications.Instance.Hub.SendFcmNativeNotificationAsync(notif);
+                    break;
+            }
+
+            if (outcome == null)
+            {
+                return new HttpResponseMessage(ret);
+            }
+
+            if (!(outcome.State == Microsoft.Azure.NotificationHubs.NotificationOutcomeState.Abandoned
+                || outcome.State == Microsoft.Azure.NotificationHubs.NotificationOutcomeState.Unknown))
+            {
+                ret = HttpStatusCode.OK;
+            }
+
+            return new HttpResponseMessage(ret);
+
+
+        }
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
